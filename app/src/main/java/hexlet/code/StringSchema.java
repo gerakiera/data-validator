31e35c2e05ba3aba9a01b8a1505daee5c07accae
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-04-29 13:58:30 +0300

    валидатор и проверка строк

---
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -0,0 +1,22 @@
+package hexlet.code;
+
+import java.util.function.Predicate;
+
+public class StringSchema extends BaseSchema<String> {
+    @Override
+    public StringSchema required() {
+        Predicate<String> req = str -> !str.isEmpty();
+        listOfPredicates.add(req);
+        return this;
+    }
+    public StringSchema minLength(int length) {
+        Predicate<String> minLen = str -> str.length() >= length;
+        listOfPredicates.add(minLen);
+        return this;
+    }
+    public StringSchema contains(String substring) {
+        Predicate<String> containsPredicate = (str) -> str.contains(substring);
+        listOfPredicates.add(containsPredicate);
+    return this;
+    }
+}
---
+++ app/src/main/java/hexlet/code/Validator.java
@@ -0,0 +1,10 @@
+package hexlet.code;
+
+public class Validator {
+    public static StringSchema string() {
+        return new StringSchema();
+    }
+    public static NumberSchema number() {
+        return new NumberSchema();
+    }
+}
