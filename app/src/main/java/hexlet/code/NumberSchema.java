38f4b477d3bfcde063b2d849bbdf095a2b0c7186
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-04-29 18:05:26 +0300

    NumberSchema

---
+++ app/src/main/java/hexlet/code/NumberSchema.java
@@ -0,0 +1,22 @@
+package hexlet.code;
+
+import java.util.function.Predicate;
+
+public class NumberSchema extends BaseSchema<Integer> {
+    @Override
+    public NumberSchema required() {
+        Predicate<Integer> req = num -> num != null;
+        listOfPredicates.add(req);
+        return this;
+    }
+    public NumberSchema positive() {
+        Predicate<Integer> pos = num -> num >= 0;
+        listOfPredicates.add(pos);
+        return this;
+    }
+    public NumberSchema range(int min, int max) {
+        Predicate<Integer> range = num -> num > min && num < max;
+        listOfPredicates.add(range);
+        return this;
+    }
+}
