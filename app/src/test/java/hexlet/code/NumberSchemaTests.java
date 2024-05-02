6445608f357b286a41dfb0b25070bcdbd5fa8c27
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-05-02 12:27:31 +0300

    NumberSchemaTests

---
+++ app/src/test/java/hexlet/code/NumberSchemaTests.java
@@ -0,0 +1,31 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NumberSchemaTests {
+    private NumberSchema n;
+    @BeforeEach
+    void beforeEach() {
+        Validator val = new Validator();
+        n = val.number();
+    }
+    @Test
+    public void testRequired() {
+        assertTrue(n.required().isValid(5));
+        assertFalse(n.required().isValid(null));
+    }
+    @Test
+    public void testPositive() {
+        assertTrue(n.positive().isValid(5));
+        assertFalse(n.positive().isValid(-1));
+    }
+    @Test
+    public void testRrange() {
+        assertTrue(n.range(1,9).isValid(5));
+        assertFalse(n.range(1,3).isValid(5));
+    }
+}
