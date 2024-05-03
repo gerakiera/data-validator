73d1e6c7d2c781d29f21439d7faa98768da34b08
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-05-03 15:17:21 +0300

    правки

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -5,17 +5,16 @@ import java.util.function.Predicate;
 public class NumberSchema extends BaseSchema<Integer> {
     @Override
     public NumberSchema required() {
-
         super.required();
         return this;
     }
     public NumberSchema positive() {
-        Predicate<Integer> pos = num -> num != null && num > 0;
+        Predicate<Integer> pos = num -> num > 0;
         listOfPredicates.add(pos);
         return this;
     }
     public NumberSchema range(int min, int max) {
-        Predicate<Integer> range = num -> num != null && num > min && num < max;
+        Predicate<Integer> range = num -> num >= min && num <= max;
         listOfPredicates.add(range);
         return this;
     }
--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -8,13 +8,15 @@ public class StringSchema extends BaseSchema<String> {
         super.required();
         return this;
     }
+
     public StringSchema minLength(int length) {
         Predicate<String> minLen = str -> str.length() >= length;
         listOfPredicates.add(minLen);
         return this;
     }
+
     public StringSchema contains(String substring) {
-        Predicate<String> containsPredicate = (str) -> str.contains(substring);
+        Predicate<String> containsPredicate = (str -> str.contains(substring));
         listOfPredicates.add(containsPredicate);
         return this;
     }
