8c861f47348272cc27fa2188658d019c06c67d9f
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-05-03 15:07:44 +0300

    добавлена проверка на null

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -5,18 +5,17 @@ import java.util.function.Predicate;
 public class NumberSchema extends BaseSchema<Integer> {
     @Override
     public NumberSchema required() {
-        /*Predicate<Integer> req = num -> num != null;
-        listOfPredicates.add(req);*/
+
         super.required();
         return this;
     }
     public NumberSchema positive() {
-        Predicate<Integer> pos = num -> num > 0;
+        Predicate<Integer> pos = num -> num != null && num > 0;
         listOfPredicates.add(pos);
         return this;
     }
     public NumberSchema range(int min, int max) {
-        Predicate<Integer> range = num -> num > min && num < max;
+        Predicate<Integer> range = num -> num != null && num > min && num < max;
         listOfPredicates.add(range);
         return this;
     }
