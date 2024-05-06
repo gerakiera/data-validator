591ead5649d44cbf8e2db5ae6330a330e9eb8851
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-05-06 11:46:14 +0300

    изменен метод required

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,12 +1,18 @@
 package hexlet.code.schemas;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public class MapSchema extends BaseSchema<Map<String, String>> {
-    @Override
+    /*@Override
     public MapSchema required() {
         super.required();
+        return this;
+    }*/
+    public MapSchema required() {
+        Predicate<Map<String,String>> required = Objects::nonNull;
+        listOfPredicates.add(required);
         return this;
     }
     public MapSchema sizeof(int size) {
