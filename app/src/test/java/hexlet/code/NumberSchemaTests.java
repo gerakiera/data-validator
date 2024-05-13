cb48cc5c749924a3e9b499b9840bb3885ef9c622
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-05-13 15:15:14 +0300

    добавлены тесты на накопление проверок и работу проверок вместе

--- app/src/test/java/hexlet/code/NumberSchemaTests.java
+++ app/src/test/java/hexlet/code/NumberSchemaTests.java
@@ -29,4 +29,21 @@ public final class NumberSchemaTests {
         assertTrue(n.range(1, 9).isValid(5));
         assertFalse(n.range(1, 3).isValid(5));
     }
+    @Test
+    public void testAccumulation() {
+        n.required();
+        assertFalse(n.isValid(null));
+        assertTrue(n.isValid(7));
+        assertTrue(n.isValid(-1));
+        n.positive();
+        n.range(2,4);
+        assertTrue(n.isValid(2));
+        assertFalse(n.isValid(1));
+    }
+    @Test
+    public void testAll() {
+        assertTrue(n.required().positive().range(2,9).isValid(7));
+        assertFalse(n.required().positive().range(5,10).isValid(2));
+        assertFalse(n.required().positive().range(5,10).isValid(-1));
+    }
 }
--- app/src/test/java/hexlet/code/StringSchemaTests.java
+++ app/src/test/java/hexlet/code/StringSchemaTests.java
@@ -31,4 +31,23 @@ public final class StringSchemaTests {
         assertTrue(s.contains("hel").isValid(str));
         assertFalse(s.contains("or").isValid(str));
     }
+    @Test
+    public void testAccumulation() {
+        String str = "hello";
+        assertTrue(s.isValid(null));
+        s.required();
+        assertFalse(s.isValid(null));
+        s.contains("hex");
+        assertFalse(s.isValid(str));
+        s.contains("ell");
+        assertTrue(s.isValid(str));
+        s.minLength(3);
+        assertTrue(s.isValid(str));
+    }
+    @Test
+    public void testAll() {
+        String str = "hello";
+        assertTrue(s.required().minLength(4).contains("hel").isValid(str));
+        assertFalse(s.required().minLength(10).contains("ex").isValid(str));
+    }
 }
